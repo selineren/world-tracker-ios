9c073257c1b0ab505452175fc1f604cac8f81f4b
Selin <selin.eren@ug.bilkent.edu.tr>
2026-02-24 23:50:03 +0300

    chore: add initial Xcode project

---
+++ WorldTrackerIOS/WorldTrackerIOS/WorldTrackerIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTrackerIOSApp.swift
+//  WorldTrackerIOS
+//
+//  Created by seren on 24.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WorldTrackerIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
