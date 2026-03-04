338f17bed92cb410cbc546852249d71b942bdaf0
Selin <selin.eren@ug.bilkent.edu.tr>
2026-03-04 21:24:06 +0300

    feat: add country centroid coordinates for map integration

--- WorldTrackerIOS/WorldTrackerIOS/Models/Continent.swift
+++ WorldTrackerIOS/WorldTrackerIOS/Models/Continent.swift
@@ -7,26 +7,26 @@
 
 import Foundation
 
-enum Continent: String, CaseIterable, Identifiable {
-    case europe
-    case asia
+enum Continent: String, CaseIterable, Identifiable, Codable {
     case africa
-    case northAmerica
-    case southAmerica
-    case australia
     case antarctica
-    
+    case asia
+    case europe
+    case northAmerica
+    case oceania
+    case southAmerica
+
     var id: String { rawValue }
-    
+
     var displayName: String {
         switch self {
-        case .europe: return "Europe"
-        case .asia: return "Asia"
         case .africa: return "Africa"
-        case .northAmerica: return "North America"
-        case .southAmerica: return "South America"
-        case .australia: return "Australia"
         case .antarctica: return "Antarctica"
+        case .asia: return "Asia"
+        case .europe: return "Europe"
+        case .northAmerica: return "North America"
+        case .oceania: return "Oceania"
+        case .southAmerica: return "South America"
         }
     }
 }
